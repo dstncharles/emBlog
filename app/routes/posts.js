4570cedaf773c4c78993e4ee3b30f7677955f907
Dustin Morando <dustinmorando@me.com>
2015-02-28 17:39:18 -0500

    fixes parse intragration

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return Ember.$.ajax("https://api.parse.com/1/classes/post").then(function(response) {
+    return Ember.$.ajax("https://api.parse.com/1/classes/Posts").then(function(response) {
       console.log(response.results);
       return response.results;
 
