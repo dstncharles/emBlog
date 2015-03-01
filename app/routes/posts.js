f269bbcc345055d7ee852176de0216682d541804
Dustin Morando <dustinmorando@me.com>
2015-02-28 19:22:08 -0500

    loged to parse

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return Ember.$.ajax("https://api.parse.com/1/classes/Posts").then(function(response) {
-      console.log(response.results);
       return response.results;
 
     });
